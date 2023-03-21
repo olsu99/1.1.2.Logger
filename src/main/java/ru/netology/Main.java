399f8c00e1513300c82455dfa533912726de83dd
olsu99 <alsou-061999@mail.ru>
2023-03-21 21:47:16 +0300

    changes

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -11,10 +11,35 @@ public class Main {
         log.log("Запускаем программу");
         Scanner scan = new Scanner(System.in);
         log.log("Просим пользователя ввести входные данные для списка");
-        System.out.println("Введите размер списка: ");
-        int listSize = Integer.parseInt(scan.nextLine());
-        System.out.println("Введите верхнюю границу для значений: ");
-        int maxValue = Integer.parseInt(scan.nextLine());
+
+        int listSize;
+        while (true) {
+            System.out.println("Введите размер списка: ");
+            try {
+                listSize = Integer.parseInt(scan.nextLine());
+                if (listSize > 0) {
+                    break;
+                }
+                log.log("Введите число больше 0");
+            } catch (Exception e) {
+                log.log("Введите числовое значение!");
+            }
+        }
+
+        int maxValue;
+        while (true) {
+            System.out.println("Введите верхнюю границу для значений: ");
+            try {
+                maxValue = Integer.parseInt(scan.nextLine());
+                if (maxValue > 0) {
+                    break;
+                }
+                log.log("Введите число больше 0");
+            } catch (Exception e) {
+                log.log("Введите числовое значение!");
+            }
+        }
+
         Random random = new Random();
         log.log("Создаём и наполняем список");
         List<Integer> list = new ArrayList<>(listSize);
@@ -24,8 +49,20 @@ public class Main {
         System.out.println("Вот случайный список: " + list.toString());
 
         log.log("Просим пользователя ввести входные данные для фильтрации");
-        System.out.println("Введите порог для фильтра");
-        int filterValue = Integer.parseInt(scan.nextLine());
+
+        int filterValue;
+        while (true) {
+            System.out.println("Введите порог для фильтра");
+            try {
+                filterValue = Integer.parseInt(scan.nextLine());
+                if (filterValue > 0) {
+                    break;
+                }
+                log.log("Введите число больше 0");
+            } catch (Exception e) {
+                log.log("Введите числовое значение");
+            }
+        }
         Filter filter = new Filter(filterValue);
         list = filter.filterOut(list);
         System.out.println("Отфильтрованный список: " + list.toString());
